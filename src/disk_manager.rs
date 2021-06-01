96654415ebb86c84614cf16aba4c3636f47b9f64
daisuzz <daisaku.rugby@gmail.com>
2021-06-01 19:55:52 +0900

    Add test for disk_manager

--- src/disk_manager.rs
+++ src/disk_manager.rs
@@ -3,7 +3,14 @@ use std::io::{Read, Seek, SeekFrom, Write};
 use std::io;
 use std::path::Path;
 
+pub const PAGE_SIZE: usize = 4096;
+
+#[derive(Copy, Clone)]
 pub struct PageId(pub u64);
+
+impl PageId {
+    pub fn to_u64(self) -> u64 { self.0 }
+}
 
 pub struct DiskManager {
     heap_file: File,
@@ -41,7 +48,7 @@ impl DiskManager {
     // データをページから読み出す
     pub fn read_page_data(&mut self, page_id: PageId, data: &mut [u8]) -> io::Result<()> {
         let offset = PAGE_SIZE as u64 * page_id.to_u64(); // オフセットの計算
-        self.heap_file.seek(SeekFrom::Start(offset)); // ファイルの先頭からoffsetバイト目(ページの先頭)
+        self.heap_file.seek(SeekFrom::Start(offset))?; // ファイルの先頭からoffsetバイト目(ページの先頭)
         self.heap_file.read_exact(data) // データの読み込み
     }
 
@@ -50,5 +57,40 @@ impl DiskManager {
         let offset = PAGE_SIZE as u64 * page_id.to_u64();  // オフセットの計算
         self.heap_file.seek(SeekFrom::Start(offset))?;  // ファイルの先頭からoffsetバイト目(ページの先頭)
         self.heap_file.write_all(data)  // データの書き込み
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tempfile::NamedTempFile;
+
+    #[test]
+    fn test() {
+        let (data_file, data_file_path) = NamedTempFile::new().unwrap().into_parts();
+        let mut disk = DiskManager::new(data_file).unwrap();
+
+        let mut hello = Vec::with_capacity(PAGE_SIZE);
+        hello.extend_from_slice(b"hello");
+        hello.resize(PAGE_SIZE, 0);
+        let hello_page_id = disk.allocate_page();
+        disk.write_page_data(hello_page_id, &hello).unwrap();
+
+        let mut world = Vec::with_capacity(PAGE_SIZE);
+        world.extend_from_slice(b"world");
+        world.resize(PAGE_SIZE, 0);
+        let world_page_id = disk.allocate_page();
+        disk.write_page_data(world_page_id, &world).unwrap();
+
+        drop(disk);
+
+        let mut disk2 = DiskManager::open(&data_file_path).unwrap();
+        let mut buf = vec![0; PAGE_SIZE];
+
+        disk2.read_page_data(hello_page_id, &mut buf).unwrap();
+        assert_eq!(hello, buf);
+
+        disk2.read_page_data(world_page_id, &mut buf).unwrap();
+        assert_eq!(world, buf);
     }
 }
